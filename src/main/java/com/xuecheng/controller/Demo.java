4c01a5747cecb7b4b08bbee8ccbcc19646941d61
guangxingwei <17777828892@163.com>
2021-08-25 14:38:52 +0800

    spring boot  demo 第一次提交

---
+++ src/main/java/com/xuecheng/controller/Demo.java
@@ -0,0 +1,14 @@
+package com.xuecheng.controller;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author mumu
+ * @date 2021/8/25 - 14:37
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaa");
+    }
+
+}
